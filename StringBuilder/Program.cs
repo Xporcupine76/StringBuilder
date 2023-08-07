c83538306f7b0ea80e0ffb221e990345aec9b0e3
STUDIO 15 <STUDIO 15@DESKTOP-HFOS1SF>
2023-08-07 09:09:18 +0200

    Add project files.

---
+++ StringBuilder/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+namespace StringBuilder01
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string s1 = "this is an example off ";
+            s1=s1 + "string concatenation";
+            Console.WriteLine(s1);
+            Console.WriteLine("==========================================");
+
+            StringBuilder sb2 = new StringBuilder("this is an example off ");
+            sb2.Append("string concatenation");
+            Console.WriteLine(sb2);
+            Console.WriteLine("==========================================");
+
+            sb2.AppendLine();
+            sb2.Append("using stringbuilder...");
+            Console.WriteLine(sb2);
+            Console.WriteLine("==========================================");
+
+            sb2.Replace("stringbuilder", "the StringBuilder class...");
+            Console.WriteLine(sb2);
+            Console.WriteLine("==========================================");  
+        }
+
+    }
+
+
+}
